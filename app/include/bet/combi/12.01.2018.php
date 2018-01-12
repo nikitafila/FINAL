c94aad09020940136d005057cc743b7da097c7ae
nikitafila <nikitafilipovich@gmail.com>
2018-01-12 18:03:43 +0300

    nikita2

---
+++ app/include/bet/combi/12.01.2018.php
@@ -0,0 +1,33 @@
+<div class="bet">
+	<span class="title">Акумулатор</span>
+	<span>КХЛ. Регулярный чемпионат.</span>
+	<span class="bold">Витязь - Автомобилист</span>
+	<span>ТБ (4.00)</span>
+	<span class="bold">1.35</span>
+</div>
+<div class="bet">
+	<span>КХЛ. Регулярный чемпионат.</span>
+	<span class="bold">Витязь - Автомобилист</span>
+	<span>ТБ (4.00)</span>
+	<span class="bold">1.35</span>
+</div>						
+<div class="bet">
+	<span>КХЛ. Регулярный чемпионат.</span>
+	<span class="bold">Витязь - Автомобилист</span>
+	<span>ТБ (4.00)</span>
+	<span class="bold">1.35</span>
+</div>
+<div class="bet">
+	<span>КХЛ. Регулярный чемпионат.</span>
+	<span class="bold">Витязь - Автомобилист</span>
+	<span>ТБ (4.00)</span>
+	<span class="bold">1.35</span>
+</div>
+<div class="result">
+	<span>Сума на залога: 50 лева</span>
+	<span>5% от банка</span>
+	<span>Общ коефициент: 3.4</span>
+	<span>Печели</span>
+	<span>Типстър: <a href="#">Веселин П</a></span>
+	<span class="small-text">11.01.2018</span>
+</div>
--- app/profi/tipster-1/index.php
+++ app/profi/tipster-1/index.php
@@ -87,75 +87,54 @@
 					</div>
 				</div>
 				<div class="stat-history">
-						<div class="bank-tip history">
-						<div class="bet">
+					<div class="bet-item">
+						<div class="bet-1">
 							<span class="title">Акумулатор</span>
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
+							<div class="single-match">
+								<span>КХЛ. Регулярный чемпионат.</span>
+								<span class="bold">Витязь - Автомобилист</span>
+								<span>ТБ (4.00)</span>
+								<span class="bold">1.35</span>
+							</div>
+							<div class="single-match">
+								<span>КХЛ. Регулярный чемпионат.</span>
+								<span class="bold">Витязь - Автомобилист</span>
+								<span>ТБ (4.00)</span>
+								<span class="bold">1.35</span>
+							</div>						
+							<div class="single-match">
+								<span>КХЛ. Регулярный чемпионат.</span>
+								<span class="bold">Витязь - Автомобилист</span>
+								<span>ТБ (4.00)</span>
+								<span class="bold">1.35</span>
+							</div>
+							<div class="single-match">
+								<span>КХЛ. Регулярный чемпионат.</span>
+								<span class="bold">Витязь - Автомобилист</span>
+								<span>ТБ (4.00)</span>
+								<span class="bold">1.35</span>
+							</div>
 						</div>
-						<div class="bet">
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
-						</div>						
-						<div class="bet">
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
-						</div>
-						<div class="bet">
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
-						</div>
-						<div class="result">
-							<span>Сума на залога: 50 лева</span>
-							<span>5% от банка</span>
-							<span>Общ коефициент: 3.4</span>
-							<span>Печели</span>
-							<span>Типстър: <a href="#">Веселин П</a></span>
-							<span class="small-text">11.01.2018</span>
+						<div class="bet-1">
+							<div class="single-match-result">
+								<span>Сума на залога: 50 лева</span>
+								<span>5% от банка</span>
+								<span>Общ коефициент: 3.4</span>
+								<span>Печели</span>
+								<span>Типстър: <a href="#">Веселин П</a></span>
+								<span class="small-text">11.01.2018</span>
+							</div>
 						</div>
 					</div>
-					<div class="bank-tip history">
-						<div class="bet">
-							<span class="title">Акумулатор</span>
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
-						</div>
-						<div class="bet">
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
-						</div>						
-						<div class="bet">
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
-						</div>
-						<div class="bet">
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
-						</div>
-					</div>
-					<div class="bank-tip history">
-						<div class="bet">
+					<div class="bet-item">
+						<div class="bet-1">
 							<span class="title">Единичен</span>
-							<span>КХЛ. Регулярный чемпионат.</span>
-							<span class="bold">Витязь - Автомобилист</span>
-							<span>ТБ (4.00)</span>
-							<span class="bold">1.35</span>
+							<div class="single-match">
+								<span>КХЛ. Регулярный чемпионат.</span>
+								<span class="bold">Витязь - Автомобилист</span>
+								<span>ТБ (4.00)</span>
+								<span class="bold">1.35</span>
+							</div>
 						</div>
 					</div>
 				</div>
