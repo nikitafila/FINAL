6708c4d6b464bd97441461f1a6c863e3323549cb
nikitafila <nikitafilipovich@gmail.com>
2018-03-16 18:37:50 +0300

    nikita-16-02

---
+++ app/bookmakers/bet365/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="bg">
+
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112407505-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-112407505-1');
+	</script>
+	<meta charset="utf-8">
+
+	<title>ТОП 5 букмейкъри за България - най-добрите сайтове за залози</title>
+	<meta name="description" content="Букмейкъри с най-високите коефициенти, 100% бонуси, бързи метози за плащане (Visa, MasterCard, Skrill, ИзиПей), онлайн съпорт, богат избор от събития.">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet"  href="/css/main.css"/>
+	<link rel="stylesheet" href="/css/menu.css">
+	<link rel="stylesheet" href="/css/buttons.css">
+	<link rel="stylesheet" href="/css/font-awesome.min.css">
+</head>
+
+<body>
+	<?php require '../../include/header.php';?>
+	<main>
+		<div class="bgr-tip">
+
+			<section class="bet-description-card">
+		          <div class="bet-description-card-item">
+		            <span class="bet-description-card-item-top">
+		              <span class="bet-description-like"></span>
+		            </span>
+		            Максимальные коэффициенты
+		            <br>Раннее открытие линий
+		          </div>
+		          <div class="bet-description-card-item">
+		            <span class="bet-description-card-item-top">
+		              <span class="bet-description-like"></span>
+		            </span>
+		            Максимальные коэффициенты
+		            <br>Раннее открытие линий
+		          </div>
+        	</section>
+
+        	<section class="rating-card">
+	          <h3 class="rating-card-title">
+	            Характеристики
+	            <span class="info">
+	              <span class="badge badge-primary">30</span>
+	            </span>
+	          </h3>
+	          <div class="rating-card-content">
+	            <div class="rating-card-content-wrapper">
+	              <div class="rating-card-item">
+	                <span class="text">Надійність</span>
+	                <span class="count">5</span>
+	              </div>
+	              <div class="progress">
+	               <div class="progress-bar bg-warning" style="width:70%;"></div>
+	              </div>
+	            </div>
+	            <div class="rating-card-content-wrapper">
+	              <div class="rating-card-item">
+	                <span class="text">Надійність</span>
+	                <span class="count">5</span>
+	              </div>
+	              <div class="progress">
+	               <div class="progress-bar bg-warning" style="width:70%;"></div>
+	              </div>
+	            </div>
+	            <div class="rating-card-content-wrapper">
+	              <div class="rating-card-item">
+	                <span class="text">Надійність</span>
+	                <span class="count">5</span>
+	              </div>
+	              <div class="progress">
+	               <div class="progress-bar bg-warning" style="width:70%;"></div>
+	              </div>
+	            </div>
+	            <div class="rating-card-content-wrapper">
+	              <div class="rating-card-item">
+	                <span class="text">Надійність</span>
+	                <span class="count">5</span>
+	              </div>
+	              <div class="progress">
+	               <div class="progress-bar bg-warning" style="width:70%;"></div>
+	              </div>
+	            </div>
+	            <div class="rating-card-content-wrapper">
+	              <div class="rating-card-item">
+	                <span class="text">Надійність</span>
+	                <span class="count">5</span>
+	              </div>
+	              <div class="progress">
+	               <div class="progress-bar bg-warning" style="width:70%;"></div>
+	              </div>
+	            </div>
+	            </div>
+          	</section>
+
+          	<table>
+	            <thead>
+	                <tr>
+	                    <th class="text-center">
+	                      <i class="fa fa-book fa-3x book-icon mb-2"></i>
+	                      <p class="text-uppercase">Продукти</p>
+	                    </th>
+	                    <th>
+	                    <i class="fa fa-desktop fa-3x book-icon mb-2"></i>
+	                      <p class="text-uppercase">Сайт</p>
+	                    </th>
+	                    <th>
+	                      <i class="fa fa-apple fa-3x book-icon mb-2"></i>
+	                      <p class="text-uppercase">IOS</p>
+	                    </th>
+	                    <th class="text-center last-column-th">
+	                      <i class="fa fa-android fa-3x book-icon mb-2"></i>
+	                      <p class="text-uppercase">Android</p>
+	                    </th>
+	                </tr>
+	            </thead>
+	            <tbody>
+	                <tr>
+	                    <th>Реєстрація</th>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-times"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Депозит</th>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-check"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Live-чат</th>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-check"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Спорт</th>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td><i class="fa fa-2x fa-times"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-check"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Live-казино</th>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-check"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Покер</th>
+	                    <td><i class="fa fa-2x fa-times"></i></td>
+	                    <td><i class="fa fa-2x fa-times"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-times"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Фінансовий беттинг</th>
+	                    <td><i class="fa fa-2x fa-times"></i></td>
+	                    <td><i class="fa fa-2x fa-times"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-times"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Кіберспорт</th>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-check"></i></td>
+	                </tr>
+	                <tr>
+	                    <th>Трансляція матчів</th>
+	                    <td><i class="fa fa-2x fa-check"></i></td>
+	                    <td><i class="fa fa-2x fa-times"></i></td>
+	                    <td class="text-center"><i class="fa fa-2x fa-check"></i></td>
+	                </tr>
+	            </tbody>
+        	</table>
+
+		</div>
+
+		<sidebar class="others-pages">
+			<section class="ad-section-main-block statii other-statii">
+				<?php require '../../include/add/ad-section-main-block-1.php';?>
+			</section>
+				<?php require '../../include/add/ad-section-main-block-2.php';?>
+		</sidebar>
+	</main>
+	<?php require '../../include/footer.php';?>
+	<script src="/js/menu.js" defer></script>
+</body>
+</html>
+
